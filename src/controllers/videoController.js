f62ea341de4a773fedbe7a4ac11efea2e1895cbb
sehun kim <cayde@Mac-mini.localdomain>
2023-06-30 12:52:50 +1200

    completed CRUD

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     return res.status(404, { error });
@@ -64,4 +64,19 @@ export const postUpload = async (req, res) => {
   }
 };
 
-export const search = (req, res) => res.send("search");
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: { $regex: new RegExp(keyword, "i") },
+    });
+  }
+  res.render("search", { pageTitle: "Search", videos });
+};
