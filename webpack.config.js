b068ef76730b549a96f3af4652d25a2139cea7c6
sehun kim <cayde@Mac-mini.localdomain>
2023-07-04 20:31:55 +1200

    setting webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path"); // path module
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// entry + output
+
+module.exports = {
+  entry: "./src/client/js/main.js", // file which will be transformed
+  mode: "development", // development or production
+  output: {
+    filename: "js/main.js", // name
+    path: path.resolve(__dirname, "assets"), // storage
+    clean: true, // clean the folder before build
+  },
+  watch: true, // watch for changes
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], // webpack takes loaders from right to left
+      },
+    ],
+  },
+};
