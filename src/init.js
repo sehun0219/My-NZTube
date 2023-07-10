1a31beac5c9dda9c73199b820708fe1d1b1a3e32
sehun kim <cayde@Mac-mini.localdomain>
2023-07-11 01:14:52 +1200

    port env add

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const handlePort = () => {
   console.log(`✅ Server is listening to http://localhost:${PORT}`);
 };
